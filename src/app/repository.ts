41a464d567d088250deba88c55cbca7e87d177e0
brayooh <brianndichu.bn@gmail.com>
2019-10-01 08:04:58 +0300

    added functions to repository class

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,5 +1,4 @@
-// export class Repository {
-//     constructor(public name:string,public html_url:string,public description:string){}
-//   }
+export class Repository {
+    constructor(public name:string,public html_url:string,public description:string){}
+  }
 
-z
