14c1858b85ca59c3abce74a101a51c1d6ffedea0
brayooh <brianndichu.bn@gmail.com>
2019-10-01 09:08:06 +0300

    added page error component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  access_token:'?access_token=8b95c9c8bdfb194bb5efa4582e40c05c6fe11377',
+  apiUrl:'https://api.github.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
+  access_token:'?access_token=8b95c9c8bdfb194bb5efa4582e40c05c6fe11377',
   apiUrl:'https://api.github.com',
-  access_token:'?access_token=8b95c9c8bdfb194bb5efa4582e40c05c6fe11377'
 };
 
 
